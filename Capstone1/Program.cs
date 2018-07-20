a6b7340723a1d0f35e5820fe6ac40eec97edecf5
Aquoinette <themedialady@blairunlimited.com>
2018-07-20 14:16:02 -0400

    vowelCommit

---
+++ Capstone1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            Console.WriteLine("Enter your word to be translated. ");
+            string userWord = Console.ReadLine();
+           
+
+            if (userWord.StartsWith("a") || userWord.StartsWith("e") || userWord.StartsWith("i") || userWord.StartsWith("o") || userWord.StartsWith("u")) 
+            {
+                Console.WriteLine(userWord + "way");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
